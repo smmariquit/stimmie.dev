7eb40cf2725e6f43d6092b2e4ec21676ecbb844b
smmariquit <smmariquit@up.edu.ph>
2026-06-30 00:50:42 +0800

    feat: add Travellings (开往) webring badge
    Required for joining the Travellings web ring. Badge is placed in:
    - Desktop sidebar (~ webring ~ box)
    - Site footer on all pages (NeoFooter + home footer)
    
    Uses the official logo.gif and links to https://www.travellings.cn/go.html

--- src/components/neo/NeoFooter.js
+++ src/components/neo/NeoFooter.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { getSiteVersion } from "@/lib/changelog";
+import TravellingsBadge from "./TravellingsBadge";
 import VisitorCounter from "./VisitorCounter";
 
 export default function NeoFooter() {
@@ -8,6 +9,9 @@ export default function NeoFooter() {
     <footer className="neo-footer mt-3">
       <div className="mx-auto mb-3 max-w-[16rem]">
         <VisitorCounter />
+      </div>
+      <div className="mb-3 flex justify-center">
+        <TravellingsBadge />
       </div>
       <p>
         made with ♥ · <Link href="/changelog">v{version}</Link> ·{" "}
